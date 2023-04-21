0b6438746a4cd1041dd185eea5adb67bd362156a
jungminyoo <jungminyoo@elicer.com>
2023-04-21 10:37:36 +0900

    Added Globalstyles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 import { ApolloProvider } from "@apollo/client";
 import client from "./client";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <RouterProvider router={router} />
+      <GlobalStyles />
     </ApolloProvider>
   );
 }
